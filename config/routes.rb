faaceb4aacdf532dd548bd305e072807a8a4b460
Mert Celebi <mertcelebi93@gmail.com>
2015-02-26 23:47:30 -0500

    Revert "as"
    This reverts commit 66afac6e9983d738a3f0293b1dd31b1e7288625b.

--- config/routes.rb
+++ config/routes.rb
@@ -109,7 +109,7 @@ TEDxYale::Application.routes.draw do
   get "/events/:year/speakers", to: "events#conference_speakers"
   
   # 2014
-  # get "/2014", to: "events#conference"
+  get "/2014", to: "events#conference"
   get "/2015", to: "applications#student_speaker_competition"
   get "/student_speaker_competition", to: "applications#student_speaker_competition"
   post "/email_nomination/:id", to: "applications#email_nomination", as: "email_nomination"
