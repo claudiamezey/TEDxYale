dcb83e808699bf731e482e78266303bc1a0d8d6b
Mert Celebi <mertcelebi93@gmail.com>
2016-02-04 17:28:32 -0500

    Bring voting to SSC back

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 TEDxYale::Application.routes.draw do
 
-  resources :sponsors, :sponsorships, :users, :applications, :sessions, :subscriptions, 
+  resources :sponsors, :sponsorships, :users, :applications, :sessions, :subscriptions,
             :application_cycles, :application_questions, :application_answers
 
   root to: "events#root"
 
   get "/join", to: "applications#new"
   get "/app_email/:id", to: "applications#app_mailer"
-  get "/signup", to: "users#signup"    
+  get "/signup", to: "users#signup"
   get "/register", to: "users#new"
   get "login", to: "sessions#new"
   get "logout", to: "sessions#destroy"
   get "/salons", to: "users#event_saman"
   get "/join", to: "users#join"
   get "/kevin_olusola_salon", to: "events#current_salon"
-      
+
   #Admin Panel
   get "/admin", to: "admin#users"
   get "/admin/users", to: "admin#lists"
@@ -31,26 +31,26 @@ TEDxYale::Application.routes.draw do
   get "/admin/competition", to: "applications#admin"
   get "/admin/student_speakers", to: "admin#student_speakers"
   get "/admin/votes", to: "admin#votes"
-  
+
   # Application Cycles
   get "/application_cycles/:id/new_question", to: "application_cycles#new_question"
   get "/apply/:slug", to: "applications#new"
-  
+
   get "promote", to: "users#promote", :as => "promote_users"
-  
+
   #Wiki
-  
+
   resources :posts
-  
+
   get "/wiki", to: "posts#index"
   get "/wiki/new", to: "posts#new"
-  
+
   #Events
-  
+
   resources :events
-  
+
   resources :attendees
-  
+
   get "/events", to: "events#index"
   get "/check_in", to: "attendees#check_in", :as => "check_in_attendees"
   get "/check_out", to: "attendees#check_out", :as => "check_out_attendees"
@@ -59,26 +59,26 @@ TEDxYale::Application.routes.draw do
   get "/events/:id/registration", to: "events#registration"
   put "/publish", to: "events#publish", :as => "publish_events"
   put "/unpublish", to: "events#unpublish", :as => "unpublish_events"
-  
+
   #About
-  
+
   get "/about", to: "about#index"
-  
-  #TV 
-  
+
+  #TV
+
   resources :videos
-  
-  get "/tv/new" => 'videos#new'  
+
+  get "/tv/new" => 'videos#new'
   get "/tv", to: "videos#index"
-  
+
   #Dashboard
-  
+
   get "/dashboard/current_events", to: "users#current_events"
   get "/dashboard/attended_events", to: "users#attended_events"
   get "/dashboard/preferences", to: "users#edit"
-  
+
   get "add_attended", to: "attendees#add_attended", :as => "add_attendees"
-  
+
   #speakers
   resources :speakers
   get "/become_a_speaker", to: "speakers#new_student"
@@ -93,10 +93,10 @@ TEDxYale::Application.routes.draw do
   get "/speaker/announcement", to: "speakers#announcement"
   get "/speaker/:id/public", to: "speakers#public"
   get "/speaker/:id/unpublic", to: "speakers#unpublic"
-  
+
   # Sponsors
   get "/sponsorships/new", to: "sponsors#new_sponsorship"
-  
+
   # 2013
   resources :student_speakers
   get "/2013", to: "events#conference"
@@ -107,22 +107,22 @@ TEDxYale::Application.routes.draw do
   get "/early_bird", to: "student_speakers#early_bird"
   get "/process_stripe", to: "student_speakers#process_stripe"
   get "/events/:year/speakers", to: "events#conference_speakers"
-  
+
   # 2014
   get "/2014", to: "events#footnotes"
   post "/email_nomination/:id", to: "applications#email_nomination", as: "email_nomination"
   get "/student_speaker_leaders", to: "student_speakers#leaders"
-  
+
   # 2015
   get "/2015", to: "events#moment_of_impact"
 
   # 2016
-  # get "/2016", to: "applications#student_speaker_competition"
-  # get "/student_speaker_competition", to: "applications#student_speaker_competition"
+  get "/2016", to: "applications#student_speaker_competition"
+  get "/student_speaker_competition", to: "applications#student_speaker_competition"
 
   # Facebook
   get 'auth/:provider/callback', to: 'sessions#create_from_facebook'
   get 'auth/failure', to: redirect('/2014')
   get 'signout', to: 'sessions#destroy', as: 'signout'
-  
+
 end
